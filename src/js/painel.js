ca55325ceab3ee096b4e024b302fb160bb64b225
TiagoSSCarneiro <tiagosergio23@gmail.com>
2022-01-16 13:57:05 -0300

    Commit para adicionar codigo JS

---
+++ src/js/painel.js
@@ -0,0 +1,44 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function esconderImagens () {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+
+}
+
+function mostrarImagens () {
+    imagensPainel[imagemAtual].classList.add ('mostrar');
+
+}
+
+setaAvancar.addEventListener('click', function () {
+    const totalDeImagens = imagensPainel.length -1;
+    if (imagemAtual === totalDeImagens) {
+        return;
+
+    }
+
+    imagemAtual++;
+
+    esconderImagens();
+    mostrarImagens ();
+    
+})
+
+setaVoltar .addEventListener ('click', function() {
+
+    if(imagemAtual === 0) {
+        return;
+    }
+
+    imagemAtual--;
+
+    esconderImagens();
+    mostrarImagens();
+
+})
+
